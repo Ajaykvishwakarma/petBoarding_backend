80844349295d9e6a6ac31de3baf013d1d3398a19
Ajaykvishwakarma <ajaykv054@gmail.com>
2022-04-23 19:57:23 +0530

    final commit

--- src/controllers/pet.controller.js
+++ src/controllers/pet.controller.js
@@ -42,17 +42,58 @@ router.get('/pet/:id', async(req, res) => {
 
 
 
-router.get('/pets', async( req, res ) => {
-    try {
-        const pet = await Pet.find().lean().exec();
-        // console.log("Hii")
+// router.get('/pets', async( req, res ) => {
+//     try {
+//         const pet = await Pet.find().lean().exec();
+//         // console.log("Hii")
        
-        return res.status(200).send({pet});
-    } catch (error) {
-        return res.status(500).send({
-            message : error.message
+//         return res.status(200).send({pet});
+//     } catch (error) {
+//         return res.status(500).send({
+//             message : error.message
+//         })
+//     }
+// }) 
+
+router.get('/pets', async (req, res) => {
+    try{
+    const page = req.query.page || 1
+    const limit = req.query.limit || 8
+    let pets = await Pet.find().skip((page - 1)  *limit).limit(limit).lean().exec()
+
+
+    const totalDocs = await Pet.find().countDocuments()   // flat.length || 0
+
+    const totalPages = (Math.ceil(totalDocs / limit))
+
+    let arr = [];
+
+    // console.log({flats})
+     for(let i = 1; i<= totalPages; i++) {
+
+        arr.push(i);
+     }
+
+        if(req.query.q) {
+            if(req.query.q == 'sort') {
+                pets = req.query.sort == 1 ? pets.sort((a, b) => (a.no - b.no)) : pets.sort((a, b) => (b.no - a.no))
+            }
+             
+            else if(req.query.q == 'filter') {
+                pets = pets.filter(pet => pet.type == req.query.base)
+            }
+            else if( req.query.q == 'search'){
+                pets = pets.filter(pet => pet.block == req.query.block)
+            }
+        }
+        // console.log({flats})
+        return res.status(200).send({
+            pets, totalPages : arr
         })
-    }
-}) 
+     
+        } catch (error ) {
+            return res.status(500).send({message : error.message})
+        }
+})
 
 module.exports = router;
